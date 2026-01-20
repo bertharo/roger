574a2a4ac2cb6595d76c17f7b6ce535ec2982c4e
bertharo <bertharo23@gmail.com>
2026-01-19 20:54:19 -0800

    Add deprecation warning suppression for url.parse() from dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Suppress url.parse() deprecation warning from dependencies
+  // This will be fixed when dependencies update to use WHATWG URL API
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Suppress the deprecation warning in server-side code
+      process.removeAllListeners('warning');
+      process.on('warning', (warning) => {
+        if (warning.name === 'DeprecationWarning' && warning.message.includes('url.parse()')) {
+          // Suppress this specific warning
+          return;
+        }
+        // Log other warnings normally
+        console.warn(warning.name, warning.message);
+      });
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
